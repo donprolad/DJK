90988518eefd16fe0666c5e10151bb3a919054f0
opstech23 <bbody8215@gmail.com>
2018-09-16 22:55:55 +0200

    Moved audio logic to playSound function, added transition logic for keydown events

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-window.addEventListener('keydown', (e) => {
+const playSound = (e) => {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
   if (!audio) return
-  audio.play(setTimeout(10))
-})
+  audio.currentTime = 0
+  audio.play()
+  key.classList.add('playing')
+}
+
+function removeTransition (e) {
+  if (e.propertyName !== 'transform') return
+  this.classList.remove('playing')
+  console.log(e.propertyName)
+}
+
+const keys = document.querySelectorAll('.key')
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+window.addEventListener('keydown', playSound)
